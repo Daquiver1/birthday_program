83e0f3c84dfabcf813726f930cfe228e37b1b7a1
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-01-09 01:35:02 +0000

    Final commit...I hope so

--- bot.py
+++ bot.py
@@ -1,34 +1,39 @@
-# Download the helper library from https://www.twilio.com/docs/python/install
-#time library to check day and month of today
-import time
-import os
+import time, os
 from twilio.rest import Client
 from twilio.http.http_client import TwilioHttpClient
 from dotenv import load_dotenv
-load_dotenv()
 
+load_dotenv()		# load the credentials
+ 
+# Twillo has issues when it's behind a proxy, this addresses the issue.
 proxy_client = TwilioHttpClient()
 proxy_client.session.proxies = {'https': os.environ['https_proxy']}
 
-# Find your Account SID and Auth Token at twilio.com/console
-# and set the environment variables. See http://twil.io/secure
+# Get credentials
 account_sid = os.getenv("account_sid")
 auth_token = os.getenv("auth_token")
 client = Client(account_sid, auth_token, http_client=proxy_client)
+
 today = time.strftime("%m-%d")
 
 def check_bday():
+	"""
+	A function to check if today is someone's birthday.
+	It reads in data from a txt file of the format 04-21 John Doe
+	It sends a message to the user.
+	"""
 	filename = open("file1.txt", "r")
 	count = 0
 
 	for line in filename:
-		if today in line:
-			count = 1 	# There is a birthday
+		if today in line:		# There is a birthday
+			count = 1 	
 			message = client.messages.create(
 			                     body=f"Today is the birthday of {line[5:]}",
 			                     from_=os.getenv("twilio_num"),
 			                     to=os.getenv("my_num"))
 			print(message.sid)
+
 	if count == 0:
 		no_birthday = client.messages.create(
 			body="There is no birthday today!",
